7182c2fc157c7897dd8cdd123b628cb5cb2d931f
Rina <cirn21059@g.nihon-u.ac.jp>
2024-10-03 18:21:03 +0900

    DBに小物追加

--- app/src/main/java/com/example/bookshelf/DatabaseHelper.java
+++ app/src/main/java/com/example/bookshelf/DatabaseHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     static final private String DB_NAME = "books_database.db";
-    static final private int VERSION = 16;
+    static final private int VERSION = 17;
 
     DatabaseHelper(Context context) {
         super(context, DB_NAME, null, VERSION);
@@ -31,15 +31,72 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 + "get INTEGER)");
 
         db.execSQL("INSERT INTO items (image, name, get)"
-                + "VALUES(" + R.drawable.shoes +",'コンバース',0)");
+                + "VALUES(" + R.drawable.filebox +",'FILEボックス',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.ipod +",'音楽プレイヤー',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.skeleton +",'ガイコツ',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.coffee_cup +",'コーヒーカップ',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.shoes +",'靴',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.bicycle +",'自転車',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.clock +",'時計',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.speaker +",'スピーカー',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.dart_arrow +",'ダーツの矢',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.dart +",'ダーツ',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.dumbbell +",'ダンベル',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.chess1 +",'チェスの駒1',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.chess2 +",'チェスの駒2',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.coffee +",'テイクアウトコーヒー',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.note +",'ノート',0)");
         db.execSQL("INSERT INTO items (image, name, get)"
                 + "VALUES(" + R.drawable.controller +",'コントローラー',0)");
         db.execSQL("INSERT INTO items (image, name, get)"
-                + "VALUES(" + R.drawable.controller +",'コントローラー2',0)");
+                + "VALUES(" + R.drawable.piggy_bank +",'ブタの貯金箱',0)");
         db.execSQL("INSERT INTO items (image, name, get)"
-                + "VALUES(" + R.drawable.controller +",'コントローラー3',0)");
+                + "VALUES(" + R.drawable.present_box +",'プレゼントボックス',0)");
         db.execSQL("INSERT INTO items (image, name, get)"
-                + "VALUES(" + R.drawable.controller +",'コントローラー4',0)");
+                + "VALUES(" + R.drawable.headphone +",'ヘッドホン',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.penguin +",'ペンギン',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.clapperboard +",'カチンコ',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.ship +",'貨物船',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.safe +",'金庫',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.hourglass1 +",'砂時計1',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.hourglass2 +",'砂時計2',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.car +",'車',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.haniwa +",'埴輪',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.torii +",'鳥居',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.light_bulb +",'電球',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.island +",'南の島',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.trophy +",'優勝カップ',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.post +",'郵便ポスト',0)");
+        db.execSQL("INSERT INTO items (image, name, get)"
+                + "VALUES(" + R.drawable.bag +",'鞄',0)");
+
     }
 
     @Override
